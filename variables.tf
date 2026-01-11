410f16b19f17a835ec0bc0cfc4058f6ed5025ec1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:43:55 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "machine_learning_compute_instances" {
+  description = <<EOT
+Map of machine_learning_compute_instances, attributes below
+Required:
+    - machine_learning_workspace_id
+    - name
+    - virtual_machine_size
+Optional:
+    - authorization_type
+    - description
+    - local_auth_enabled
+    - node_public_ip_enabled
+    - subnet_resource_id
+    - tags
+    - assign_to_user (block):
+        - object_id (optional)
+        - tenant_id (optional)
+    - identity (block):
+        - identity_ids (optional)
+        - type (required)
+    - ssh (block):
+        - public_key (required)
+EOT
+
+  type = map(object({
+    machine_learning_workspace_id = string
+    name                          = string
+    virtual_machine_size          = string
+    authorization_type            = optional(string)
+    description                   = optional(string)
+    local_auth_enabled            = optional(bool, true)
+    node_public_ip_enabled        = optional(bool, true)
+    subnet_resource_id            = optional(string)
+    tags                          = optional(map(string))
+    assign_to_user = optional(object({
+      object_id = optional(string)
+      tenant_id = optional(string)
+    }))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    ssh = optional(object({
+      public_key = string
+    }))
+  }))
+}
+
